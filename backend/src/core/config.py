54285fe3e0b66ae0e1fe6ec8aa5b438056c1f416
Valentina Saccone <valesaccone20@gmail.com>
2026-01-27 21:08:09 -0300

    Revert "Agregar configuraciones"
    This reverts commit a76ce7af60895f885cdf6695b1c7d040e2284223.

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -3,8 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     APP_NAME: str = "Backend Gestion Documental"
     APP_VERSION: str = "1.0.0"
-    #DATABASE_URL: str = "postgresql+psycopg2://admin:admin123@localhost:5432/gestion_documental_db"
-    DATABASE_URL: str
+    DATABASE_URL: str = "postgresql+psycopg2://admin:admin123@localhost:5432/gestion_documental_db"
 
     class Config:
         env_file = ".env"
